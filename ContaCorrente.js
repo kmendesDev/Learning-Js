658c4d89ceeed19d41c088c4d1f3e72965ba350b
Kaio Mendes <kaio.mendes@bb.com.br>
2024-07-04 13:12:23 -0300

    Separação de classes em arquivos

---
+++ ContaCorrente.js
@@ -0,0 +1,45 @@
+
+import {Cliente} from "./Cliente.js" 
+
+export class ContaCorrente {
+    agencia;
+    _cliente;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+    // Atributo privado: #saldo
+    // Convenção atual:
+    _saldo = 0;
+    // MÉTODO:
+    sacar(valor) {
+        if (this._saldo >= valor) {
+            this._saldo -= valor;
+            console.log("Saque realizado")
+            console.log("Saldo atual:", this._saldo);
+            return valor;
+        }
+        else {
+            console.log("Saque não autorizado")
+            console.log("Saldo atual:", this._saldo);
+        }
+    }
+
+    depositar(valor) {
+        if (valor > 0) {
+            this._saldo += valor;
+            console.log("Depósito realizado")
+        }
+        else {
+            console.log("Depósito não autorizado")
+        }
+        console.log("Saldo atual:", this._saldo);
+    }
+
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
+    }
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
+const cliente1 = new Cliente();
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+
+// console.log(cliente1);
+// console.log(cliente2);
+
+const contaCorrenteRicardo = new ContaCorrente();
+//contaCorrenteRicardo.saldo = 10000;
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+
+contaCorrenteRicardo.depositar(500);
+
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia=1002;
+
+let valorTransf = 300
+contaCorrenteRicardo.transferir(valorTransf, conta2);
+
+console.log(contaCorrenteRicardo)
+console.log(conta2);
+
+
+
+// const valorSacado = contaCorrenteRicardo.sacar(50)
+// console.log("Saque",valorSacado)
+
+
